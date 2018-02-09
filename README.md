9744c46ab446ba010a350b2303b82728e2cfb9fe
rtatol <rtatol@gmail.com>
2018-02-09 21:18:20 +0100

    update maven compiler plugin configuration

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # codility-solutions
 codility lessons solved in Java
+
+https://app.codility.com/programmers/lessons
+
+Only 100/100 solutions.
--- src/test/java/com/codility/frogJmp/SolutionTest.java
+++ src/test/java/com/codility/frogJmp/SolutionTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+// FrogJmp
+// https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/
 public class SolutionTest {
 
     private Solution solution = new Solution();
